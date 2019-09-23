2f1e44c3358ce5b07dbdca8037dacbce07647826
EBouzbib <elo.bouzbib@gmail.com>
2019-09-23 14:17:22 +0200

    Calibration 1 fois

--- Transfer Retargeting - VIVE/Assets/Retargeting/Scripts/Pseudo-haptic/BodyWarping.cs
+++ Transfer Retargeting - VIVE/Assets/Retargeting/Scripts/Pseudo-haptic/BodyWarping.cs
@@ -18,18 +18,14 @@ public class BodyWarping : MonoBehaviour {
         
     }
 
-    public Vector3 BodyWarp(Vector3 realPosition, Vector3 wOrigin, Vector3 wTargetReal, Vector3 wTargetVirtual) {
-        Vector3 realHandPos = realPosition;
+    public Vector3 BodyWarp(Vector3 realHandPos, Vector3 wOrigin, Vector3 wTargetReal, Vector3 wTargetVirtual) {
         Vector3 d = realHandPos - wTargetReal;
         Vector3 D = wOrigin - wTargetReal;
         Vector3 virtHandPos = realHandPos;
 
         if (d.magnitude > D.magnitude) {
-            warp = false;
             return realHandPos;
-        }
-        else {
-            warp = true;
+        } else {
             Vector3 lambda = wTargetVirtual - wTargetReal;
 
             virtHandPos = (D.magnitude - d.magnitude) / D.magnitude * lambda + realHandPos;
