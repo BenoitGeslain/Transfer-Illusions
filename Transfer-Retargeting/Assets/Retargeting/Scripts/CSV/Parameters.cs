7365d6e05cc179a66ac4eb3ad8ee6d9afb5fffb9
benoitgeslain <benoitgeslain@gmail.com>
2019-10-09 14:39:54 +0200

    Logging headers corrected

--- Transfer-Retargeting/Assets/Retargeting/Scripts/CSV/Parameters.cs
+++ Transfer-Retargeting/Assets/Retargeting/Scripts/CSV/Parameters.cs
@@ -7,7 +7,7 @@ public static class Parameters {
 	public static int blockFactor = 100;
 	public static int trialFactor = 10000;
 
-    public static string[] tracebackHeader = {"Participant", "Practice", "Block", "Trial", "Condition", "Time", "Index", "CubePosRX", "CubePosRY", "CubePosRZ", "CubeOrRX", "CubeOrRY", "CubeOrRZ", "CubePosVX", "CubePosVY", "CubePosVZ", "CubeOrVX", "CubeOrVY", "CubeOrVZ", "Score"};
-    public static string[] experimentHeader = {"Participant", "Practice", "Block", "Trial", "Index", "ErrorPosVX", "ErrorPosVY", "ErrorPosVZ", "ErrorOr", "Hit", "Score"};
+    public static string[] tracebackHeader = {"Participant", "Practice", "Block", "Trial", "Condition", "Time", "Index", "CubePosRX", "CubePosRY", "CubePosRZ", "CubeOrRX", "CubeOrRY", "CubeOrRZ", "CubePosVX", "CubePosVY", "CubePosVZ", "CubeOrVX", "CubeOrVY", "CubeOrVZ", "Acceleration", "collisions1", "collisions2", "collisions3", "collisions4", "score"};
+    public static string[] experimentHeader = {"Participant", "Practice", "Block", "Trial", "Condition", "ElapsedTime", "TrialStartTime" "Index", "ErrorPosVX", "ErrorPosVY", "ErrorPosVZ", "ErrorOr", "Hit", "collisions1", "collisions2", "collisions3", "collisions4", "Score"};
     public static string[] sceneConfiguration = {"Name", "PX", "PY", "PZ", "RX", "RY", "RZ"};
 }
